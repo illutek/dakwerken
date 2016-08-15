d9882b8272871c97166a80997d139dc7ca30e721
Vandenborne Stefan <stefan@illutek.be>
2016-08-15 20:39:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stefan
+ * Date: 15/08/2016
+ * Time: 13:53
+ */?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="vendor/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="vendor/animate.css/animate.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="vendor/jquery/dist/jquery.js"></script>
+  <script src="vendor/bootstrap/dist/js/bootstrap.js"></script>
+  <script src="vendor/jquery.stellar/jquery.stellar.js"></script>
+  <script src="vendor/wow/dist/wow.js"></script>
+  <script>new WOW().init(); </script>
+
+
+  <title>Dakwerken Daniels</title>
+</head>
+<body>
+<div class="">
+  <img src="images/layout.jpg">
+</div>
+</body>
+</html>

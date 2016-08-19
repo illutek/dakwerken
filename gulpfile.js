149d615d2bf02735d9b5905ab49b4df697da125f
Vandenborne Stefan <stefan@illutek.be>
2016-08-19 22:36:55 +0200

    Gulp.js toegevoegd

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+//var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+//var imagemin = require('gulp-imagemin');
+//var cache = require('gulp-cache');
+
+// Sass to css
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src(['css/*.css', '!css/*.min.css'])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'));
+});
+
+// gulp.task('minify-js', function () {
+//    gulp.src('src/**/*.js')
+//        .pipe(jsmin())
+//        .pipe(rename({suffix: '.min'}))
+//        .pipe(gulp.dest('dist'));
+// });
+
+// //////////////////////////////////////////////////////////
+// Task Image minify TODO werkt nog niet verder uitzoeken
+// //////////////////////////////////////////////////////////
+
+//gulp.task('images', function(){
+//    return gulp.src('pre-images/**/*')
+//        .pipe(imagemin({ progressive: true}))
+//        .pipe(gulp.dest('images'));
+//});
+
+// ///////////////////////////////////////////////////
+// Watch Task
+// ///////////////////////////////////////////////////
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', ['sass']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch()
+});
+// ///////////////////////////////////////////////////
+// Default Task
+// ///////////////////////////////////////////////////
+
+gulp.task('default' , ['watch', 'sass', 'minify-css']);
